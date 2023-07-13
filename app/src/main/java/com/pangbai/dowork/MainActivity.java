74c3a7e4b53084b14fa660d124761eb72104d9aa
15183252672 <15183252672@163.com>
2023-07-13 17:30:00 +0800

    none

--- app/src/main/java/com/pangbai/dowork/MainActivity.java
+++ app/src/main/java/com/pangbai/dowork/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pangbai.dowork;
 
+import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 import com.pangbai.dowork.databinding.ActivityMainBinding;
 import com.pangbai.dowork.layout.FragmentAdapter;
@@ -22,53 +24,59 @@ import com.pangbai.view.CustomDialog;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-    private ActivityMainBinding binding;
+public class MainActivity extends AppCompatActivity
+    implements View.OnClickListener, BottomNavigationView.OnNavigationItemSelectedListener {
+  private TabLayout tabLayout;
+  private ViewPager viewPager;
+  private ActivityMainBinding binding;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-      util.fullScreen(getWindow(),false);
-        new Init(MainActivity.this);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-       // tabLayout = binding.tab;
-        //viewPager = binding.tabPager;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    util.fullScreen(getWindow(), false);
+    new Init(MainActivity.this);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+    // tabLayout = binding.tab;
+    // viewPager = binding.tabPager;
 
+    binding.navView.setOnNavigationItemSelectedListener(this);
 
-       binding.button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                util.startActivity(MainActivity.this,TermActivity.class,false);
-            }
+    binding.button.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            util.startActivity(MainActivity.this, TermActivity.class, false);
+          }
         });
+  }
+
+  public void setTablayout() {
+    List<String> title = new ArrayList<>();
+    List<Fragment> fragments = new ArrayList<>();
+    title.add("Dashboard");
+    title.add("Container");
+    title.add("Xserver");
+    title.add("Terminal");
+    for (String str : title) {
+      tabLayout.addTab(tabLayout.newTab().setText(str));
     }
 
-
-    public  void  setTablayout(){
-        List<String> title=new ArrayList<>();
-        List<Fragment> fragments=new ArrayList<>();
-        title.add("Dashboard");
-        title.add("Container");
-        title.add("Xserver");
-        title.add("Terminal");
-        for (String str:title){
-            tabLayout.addTab(tabLayout.newTab().setText(str));
-        }
-
-        for (int i=0;i<title.size();i++){
-            fragments.add(new ListFragment());
-        }
-        FragmentAdapter fragmentAdapter=new FragmentAdapter(getSupportFragmentManager(),fragments,title);
-        viewPager.setAdapter(fragmentAdapter);
-        tabLayout.setupWithViewPager(viewPager);
-        tabLayout.setTabsFromPagerAdapter(fragmentAdapter);
-}
-
-    @Override
-    public void onClick(View view) {
-
+    for (int i = 0; i < title.size(); i++) {
+      fragments.add(new ListFragment());
     }
+    FragmentAdapter fragmentAdapter =
+        new FragmentAdapter(getSupportFragmentManager(), fragments, title);
+    viewPager.setAdapter(fragmentAdapter);
+    tabLayout.setupWithViewPager(viewPager);
+    tabLayout.setTabsFromPagerAdapter(fragmentAdapter);
+  }
+
+  @Override
+  public void onClick(View view) {}
+
+  @Override
+  public boolean onNavigationItemSelected(MenuItem arg0) {
+      return true;
+  }
 }
