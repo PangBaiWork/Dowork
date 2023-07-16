e47c8025e1604300515132b269ac103ecad3a57e
15183252672 <15183252672@163.com>
2023-07-16 18:56:33 +0800

    null1

--- app/src/main/java/com/pangbai/dowork/MainActivity.java
+++ app/src/main/java/com/pangbai/dowork/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity
     binding.navView.setOnItemSelectedListener(this);
    // binding.navView.setupWithNavController();
         
+        
     /*binding.button.setOnClickListener(
         new View.OnClickListener() {
           @Override
