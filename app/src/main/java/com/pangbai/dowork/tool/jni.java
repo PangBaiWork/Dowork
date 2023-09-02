ef056829e2565f194e8585e0c0c7c1a06630c49d
PangBaiWork <15183252672@163.com>
2023-09-02 18:58:16 +0800

    version1.0

--- app/src/main/java/com/pangbai/dowork/tool/jni.java
+++ app/src/main/java/com/pangbai/dowork/tool/jni.java
@@ -9,10 +9,10 @@ public class jni {
     public static native String xclick(int x,int y,boolean isleft);
   //  public  static native String inputKey(int x,String s);
 
-    public  static native int initxvfb(String screen);
+ //   public  static native int initxvfb(String screen);
 
     public static native void stopDraw();
-    public  static native int startdraw(Surface face,int a);
+   // public  static native int startdraw(Surface face,int a);
     public static  native  int fullScreen(int height,int width);
     public static  native  int mouseScroll(boolean up);
     public static native int inputString(String str);
@@ -21,17 +21,14 @@ public class jni {
     public static native int cursorControl(int statue,int x,int y);
 
     static {
-//        System.loadLibrary("android-ash");
+
         System.loadLibrary("Xau");
         System.loadLibrary("Xdmcp");
         System.loadLibrary("xdraw");
-        System.loadLibrary("bz2");
-
-
+        //        System.loadLibrary("android-ash");
+//        System.loadLibrary("bz2");
         //	System.loadLibrary("crypto");
-
-
-        System.loadLibrary("xserver");
+ //       System.loadLibrary("xserver");
 
     }
 }
